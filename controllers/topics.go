67ef1403966c803b20e1d5cb4d342f33a4311e0b
Li Yuqing <im.yuqlee@gmail.com>
2018-11-29 23:23:46 +0800

    Add update topic router

--- controllers/topics.go
+++ controllers/topics.go
@@ -12,7 +12,7 @@ import (
 	"github.com/godiscourse/godiscourse/views"
 )
 
-type topicRequet struct {
+type topicRequest struct {
 	Title      string `json:"title"`
 	Body       string `json:"body"`
 	CategoryID string `json:"category_id"`
@@ -24,18 +24,32 @@ func registerTopic(router *httptreemux.TreeMux) {
 	impl := &topicImpl{}
 
 	router.POST("/topics", impl.create)
+	router.POST("/topics/:id", impl.update)
 	router.GET("/topics/:id", impl.show)
 	router.GET("/topics", impl.index)
 	router.GET("/user/topics", impl.topics)
 }
 
 func (impl *topicImpl) create(w http.ResponseWriter, r *http.Request, _ map[string]string) {
-	var body topicRequet
+	var body topicRequest
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		views.RenderErrorResponse(w, r, session.BadRequestError(r.Context()))
 		return
 	}
 	if topic, err := middleware.CurrentUser(r).CreateTopic(r.Context(), body.Title, body.Body, body.CategoryID); err != nil {
+		views.RenderErrorResponse(w, r, err)
+	} else {
+		views.RenderTopic(w, r, topic)
+	}
+}
+
+func (impl *topicImpl) update(w http.ResponseWriter, r *http.Request, params map[string]string) {
+	var body topicRequest
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		views.RenderErrorResponse(w, r, session.BadRequestError(r.Context()))
+		return
+	}
+	if topic, err := middleware.CurrentUser(r).UpdateTopic(r.Context(), params["id"], body.Title, body.Body); err != nil {
 		views.RenderErrorResponse(w, r, err)
 	} else {
 		views.RenderTopic(w, r, topic)
