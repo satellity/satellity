0d3d3e7f8d7b6126dc7281f386228b338d650e7b
Li Yuqing <im.yuqlee@gmail.com>
2019-03-30 13:08:16 +0800

    Fix deploy_web_example, should change .env

--- deploy/deploy_web_example.sh
+++ deploy/deploy_web_example.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cd path/to/godiscourse/web || exit
+cp .env.production .env || exit
 rm -rf dist || exit
 npm run build || exit
 rsync -rcv dist/* remote.server.host:path/to/godiscourse/html/ || exit
+cp .env.development .env || exit
