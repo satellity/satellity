cd132039d12a321852d20aaa1b4d1b25f680b622
Li Yuqing <im.yuqlee@gmail.com>
2019-07-01 00:23:37 +0800

    Add: more routes for messages

--- internal/controllers/message.go
+++ internal/controllers/message.go
@@ -24,6 +24,8 @@ type messageImpl struct {
 func registerMessage(database *durable.Database, router *httptreemux.TreeMux) {
 	impl := &messageImpl{database: database}
 	router.POST("/groups/:id/messages", impl.create)
+	router.POST("/messages/:id", impl.update)
+	router.POST("/messages/:id/delete", impl.destroy)
 	router.GET("/groups/:id/messages", impl.index)
 }
 
@@ -40,6 +42,38 @@ func (impl *messageImpl) create(w http.ResponseWriter, r *http.Request, params m
 		views.RenderErrorResponse(w, r, err)
 	} else {
 		views.RenderMessage(w, r, message)
+	}
+}
+
+func (impl *messageImpl) update(w http.ResponseWriter, r *http.Request, params map[string]string) {
+	var body messageRequest
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		views.RenderErrorResponse(w, r, err)
+		return
+	}
+
+	mctx := models.WrapContext(r.Context(), impl.database)
+	message, err := middleware.CurrentUser(r).UpdateMessage(mctx, params["id"], body.Body)
+	if err != nil {
+		views.RenderErrorResponse(w, r, err)
+	} else {
+		views.RenderMessage(w, r, message)
+	}
+}
+
+func (impl *messageImpl) destroy(w http.ResponseWriter, r *http.Request, params map[string]string) {
+	var body messageRequest
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		views.RenderErrorResponse(w, r, err)
+		return
+	}
+
+	mctx := models.WrapContext(r.Context(), impl.database)
+	err := middleware.CurrentUser(r).DeleteMessage(mctx, params["id"])
+	if err != nil {
+		views.RenderErrorResponse(w, r, err)
+	} else {
+		views.RenderBlankResponse(w, r)
 	}
 }
 
