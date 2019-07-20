c526fce8b6efc67a481fc961021eb3d803e6339e
Li Yuqing <im.yuqlee@gmail.com>
2019-07-20 17:00:26 +0800

    Fix dashboard access permission

--- web/src/home/dashboard.js
+++ web/src/home/dashboard.js
@@ -1,6 +1,6 @@
 import style from './dashboard.scss';
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import API from '../api/index.js';
 import TopicItem from '../topics/item.js';
 import GroupItem from '../groups/item.js';
@@ -9,24 +9,33 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.api = new API();
+    let user = this.api.user.readMe();
     this.state = {
+      user: user,
       groups: [],
       topics: []
     }
   }
 
   componentDidMount() {
-    let user = this.api.user.readMe();
-    this.api.user.topics(user.user_id).then((data) => {
-      this.setState({topics: data});
-    });
-    this.api.me.groups(3).then((data) => {
-      this.setState({groups: data});
-    });
+    if (!!this.state.user.user_id) {
+      this.api.user.topics(this.state.user.user_id).then((data) => {
+        this.setState({topics: data});
+      });
+      this.api.me.groups(3).then((data) => {
+        this.setState({groups: data});
+      });
+    }
   }
 
   render() {
     let state = this.state;
+    if (!state.user.user_id) {
+      return (
+        <Redirect to={{ pathname: "/" }} />
+      )
+    }
+
     const groups = state.groups.map((group) => {
       return (
         <div key={group.group_id} className={style.group}>
