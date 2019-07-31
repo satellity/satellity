9d40eb4c7f4f5b2a65d0e1b42d97710b1b01eafd
Yuqing Lee <im.yuqlee@gmail.com>
2019-08-01 01:51:10 +0800

    Fix texts

--- web/src/home/index.js
+++ web/src/home/index.js
@@ -20,19 +20,19 @@ class Index extends Component {
     return (
       <div>
         <h1 className={style.slogan}>
-          {i18n.t('site.slogan')}
+          {i18n.t('site.features')}
         </h1>
         <div className={style.features}>
-          <div className={style.section}>
-            <FontAwesomeIcon icon={['fa', 'chalkboard']} />
-            <div className={style.desc}>
-              {i18n.t('home.forum')}
-            </div>
-          </div>
           <div className={style.section}>
             <FontAwesomeIcon icon={['fa', 'users-cog']} />
             <div className={style.desc}>
               {i18n.t('home.group')}
+            </div>
+          </div>
+          <div className={style.section}>
+            <FontAwesomeIcon icon={['fa', 'chalkboard']} />
+            <div className={style.desc}>
+              {i18n.t('home.forum')}
             </div>
           </div>
         </div>
