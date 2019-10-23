e9446327103880c666e53325c2f6ce62ff94a55b
Yuqing Lee <im.yuqlee@gmail.com>
2019-10-23 23:40:41 +0800

    Fix README

--- deploy/deploy_web_example.sh
+++ deploy/deploy_web_example.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-cd path/to/satellity/web || exit
-cp .env.production .env || exit
-rm -rf dist || exit
-npm run build || exit
-rsync -rcv --delete dist/ remote.server.host:path/to/satellity/html/ || exit
-cp .env.development .env || exit
+cd path/to/satellity/app || exit
+yarn install || exit
+rm -rf build || exit
+yarn build || exit
+rsync -rcv --delete build/ remote.server.host:path/to/satellity/html/ || exit
