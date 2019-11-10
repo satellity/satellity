738d610f8b05318973129e50991492ee401a79b3
Yuqing Lee <im.yuqlee@gmail.com>
2019-11-10 18:23:17 +0800

    Fix text

--- app/src/admin/index.js
+++ app/src/admin/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Config from '../components/config.js';
 
 class Index extends Component {
   render() {
     return (
       <h1 className='welcome'>
-        This is the Dashboard for GoDiscourse.
+          This is the Dashboard for {Config.Name}.
       </h1>
     )
   }
