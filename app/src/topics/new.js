26e08994fcd8fd88f70fbcd56aab7c67b2511dc5
Li Yuqing <im.yuqlee@gmail.com>
2020-06-05 23:59:03 +0800

    Fix text

--- app/src/topics/new.js
+++ app/src/topics/new.js
@@ -305,7 +305,7 @@ class New extends Component {
           </div>
         }
         <div className={style.upload}>
-          <a href='https://imgur.com/upload' target='_blank' rel='noopener noreferrer'>Does not support uploading pictures, please use imgur first.</a>
+          <a href='https://imgur.com/upload' target='_blank' rel='noopener noreferrer'>Does not support upload image, please use imgur first.</a>
         </div>
         {
           state.topic_type === 'LINK' &&
