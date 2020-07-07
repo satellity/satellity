53fb8b966b4253b54fb75b03040ae2d6da2a3a7c
Li Yuqing <im.yuqlee@gmail.com>
2020-07-07 22:55:31 +0800

    Slight fix: css

--- app/src/products/index.js
+++ app/src/products/index.js
@@ -1,5 +1,5 @@
 import style from './index.module.scss';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import LazyLoad from 'react-lazyload';
 
