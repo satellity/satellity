d0c0b3b1044cb51cc3d11846e218a8fe43ca45fa
Li Yuqing <im.yuqlee@gmail.com>
2020-08-03 23:11:06 +0800

    Fix access deny from github

--- app/src/users/oauth.js
+++ app/src/users/oauth.js
@@ -1,5 +1,6 @@
 import style from './oauth.module.scss';
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import API from '../api/index.js';
 import Loading from '../components/loading.js';
 
@@ -13,21 +14,24 @@ class Oauth extends Component {
     let params = new URLSearchParams(props.location.search);
     this.state = {
       code: params.get('code'),
+      redirect: false,
     };
   }
 
   componentDidMount() {
     // TODO should use redirect
     const props = this.props;
-    new API().user.signIn('','', props.match.params.provider,this.state.code).then((resp) => {
-      if (resp.error) {
-        return
-      }
-      props.history.push('/');
+    new API().user.signIn('','',props.match.params.provider,this.state.code).then((resp) => {
+      this.setState({redirect: true});
     });
   }
 
   render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect to={{ pathname: "/" }} />
+      )
+    }
     return (
       <div className={style.loading}>
         <Loading class='default' />
