4ba0f72b4de664c190da6140ecf15290be785a00
Li Yuqing <im.yuqlee@gmail.com>
2020-08-18 22:52:08 +0800

    Unique tags

--- app/src/api/product.js
+++ app/src/api/product.js
@@ -20,25 +20,25 @@ class Admin {
   }
 
   create(params) {
-    const tags = params.tags.map((e) => {
+    const tags = Array.from(new Set(params.tags.map((e) => {
       if (typeof e !== 'string') return e;
       return e.trim().replace(/\s+/g, ' ').split(' ').map((c) => {
         c = c.trim()
         return c.charAt(0).toUpperCase() + c.slice(1);
       }).join(' ');
-    });
+    })));
     const data = {name: params.name, body: params.body, cover: params.cover, source: params.source, tags: tags};
     return this.api.post('/admin/products', data);
   }
 
   update(params) {
-    const tags = params.tags.map((e) => {
+    const tags = Array.from(new Set(params.tags.map((e) => {
       if (typeof e !== 'string') return e;
       return e.trim().replace(/\s+/g, ' ').split(' ').map((c) => {
         c = c.trim()
         return c.charAt(0).toUpperCase() + c.slice(1);
       }).join(' ');
-    });
+    })));
     const data = {name: params.name, body: params.body, cover: params.cover, source: params.source, tags: tags};
     return this.api.post(`/admin/products/${params.product_id}`, data);
   }
