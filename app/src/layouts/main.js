3e111b68eb76c05dfeb0d1ccefdf8c0321f6ba6c
LiYuqing <im.yuqlee@gmail.com>
2022-09-01 23:23:57 +0800

    Fix man layout

--- app/src/layouts/main.js
+++ app/src/layouts/main.js
@@ -1,29 +1,14 @@
-import style from './main.module.scss';
-import React, {Component} from 'react';
-import {Routes, Outlet} from 'react-router-dom';
+import React from 'react';
+import {Outlet} from 'react-router-dom';
 import Header from './header.js';
 
-class MainLayout extends Component {
-  constructor(props) {
-    super(props);
-
-    const classes = document.body.classList.values();
-    document.body.classList.remove(...classes);
-    this.state = {p: ''};
-  }
-
-  render() {
-    return (
-      <div className={style.container}>
-        <Header />
-        <div className='wrapper'>
-          <Outlet />
-          <Routes>
-          </Routes>
-        </div>
-      </div>
-    );
-  }
-}
+const MainLayout = () => (
+  <>
+    <Header />
+    <div className='wrapper'>
+      <Outlet />
+    </div>
+  </>
+);
 
 export default MainLayout;
