d7cc9646387245925457e9182575fd9f5494e5be
LiYuqing <im.yuqlee@gmail.com>
2022-10-13 11:19:23 +0800

    Show ndoes

--- app/src/gists/layout.js
+++ app/src/gists/layout.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Chart from './chart.js';
+import {useParams, Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Widget from 'components/widget.js';
+import Chart from './chart.js';
+
+import style from './index.module.scss';
 
 const View = ({children}) => {
+  const {genre} = useParams();
+  console.log(genre);
+
   return (
     <div className='container'>
       <main className='column main'>
+        <div className={style.nodes}>
+          <Link to="/" className={`${style.node} ${!genre ? style.current : ''}`}> News </Link>
+          <Link to="/genres/release" className={`${style.node} ${genre === 'release' ? style.current : ''}`}> Releases </Link>
+        </div>
         {children}
       </main>
       <aside className='column aside'>
